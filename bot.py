dc4afa9adb1234aed9d5697cdf49b916718dc79f
Ema639 <168801762+Ema639@users.noreply.github.com>
2024-10-15 19:15:23 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,35 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram import F
+import asyncio
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+API_TOKEN = '8128564762:AAEV6TADhd_U6bovm1fiPXjNKgMp6daCDOs'
+
+# Список ключевых слов для фильтрации
+spam_keywords = ['доход', 'заработок']
+
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(F.text)
+async def filter_spam(message: types.Message):
+    # Проверка на наличие ключевых слов в сообщении
+    if any(keyword in message.text.lower() for keyword in spam_keywords):
+        await message.delete()  # Удаление сообщения
+        print(f"Удалено сообщение от {message.from_user.full_name}: {message.text}")
+
+
+async def main():
+    await dp.start_polling(bot)
+    await bot.delete_webhook(drop_pending_updates=True)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
